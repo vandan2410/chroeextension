69c374a2fc7e49247c04142fe517fc9b1d703623
vandan2410 <89760982+vandan2410@users.noreply.github.com>
2023-05-16 19:20:33 +0530

    url logged

--- dist/content.js
+++ dist/content.js
@@ -1,6 +1,16 @@
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   if (request.action === 'getSelectedText') {
+//     var selectedText = window.getSelection().toString();
+//     sendResponse(selectedText);
+//   }
+// });
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === 'getSelectedText') {
     var selectedText = window.getSelection().toString();
-    chrome.runtime.sendMessage({ action: 'sendSelectedText', text: selectedText });
+    var tabUrl = window.location.href;
+    sendResponse({ text: selectedText, url: tabUrl });
   }
 });
+
+
+
--- public/background.js
+++ public/background.js
@@ -1,26 +1,55 @@
+// chrome.commands.onCommand.addListener(function (command) {
+//   if (command === 'logSelectedText') {
+//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//       var tabId = tabs[0].id;
+//       chrome.scripting.executeScript({
+//         target: { tabId: tabId },
+//         function: logSelectedText,
+//       });
+//     });
+//   }
+// });
+
+// function logSelectedText() {
+//   var selectedText = window.getSelection().toString();
+//   chrome.runtime.sendMessage({ action: 'sendSelectedText', text: selectedText });
+// }
+
+
+
+
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   if (request.action === 'sendSelectedText') {
+//     var selectedText = request.text;
+//     console.log(selectedText);
+//   }
+// });
+
 chrome.commands.onCommand.addListener(function (command) {
   if (command === 'logSelectedText') {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      var tabId = tabs[0].id;
+      var tab = tabs[0];
       chrome.scripting.executeScript({
-        target: { tabId: tabId },
-        function: logSelectedText,
+        target: { tabId: tab.id },
+        files: ['content.js'],
+      }, function () {
+        chrome.tabs.sendMessage(tab.id, { action: 'getSelectedText' }, function (response) {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+            return;
+          }
+          var selectedText = response?.text || '';
+          var tabUrl = response?.url || '';
+          console.log('Selected Text:', selectedText);
+          console.log('Site URL:', tabUrl);
+        });
       });
     });
   }
 });
 
-function logSelectedText() {
-  var selectedText = window.getSelection().toString();
-  chrome.runtime.sendMessage({ action: 'sendSelectedText', text: selectedText });
-}
 
 
 
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === 'sendSelectedText') {
-    var selectedText = request.text;
-    console.log(selectedText);
-  }
-});
+
